0e169506b052a3d377a55142cb4bbb0cb2d5bd61
Alex Thorburn-Winsor <sputnik4024@gmail.com>
2021-05-13 17:00:14 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,16 @@ axios.get('https://www.reddit.com/r/javascript.json', {
   
 });
 
+fetch('https://api.openuv.io/api/v1/uv?lat=-31.45&lng=115.67&dt=2018-01-24T10:50:52.283Z', {
+  headers: {
+      'x-access-token': '1d98887a2e12a06a3155e96808793b5e'
+    },
+})
+  .then(response => response.json())
+  .then(json => console.log(json))
+
+let output=json;
+
 import Link from 'next/link'
 const Index = () => (
   <div>

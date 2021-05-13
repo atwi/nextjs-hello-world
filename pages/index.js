29419692f65485184de70ae4f6d2bcecf71d63c3
Alex Thorburn-Winsor <sputnik4024@gmail.com>
2021-05-13 16:42:45 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,8 @@
 import axios from 'axios';
 
-function getUVIndex() {
- var lat = $('#lat').val();
- var lng = $('#lng').val();
- var alt = $('#alt').val();
- var ozone = $('#ozone').val();
- var dt = $('#dt').val();
-
- $.ajax({
-   type: 'GET',
-   dataType: 'json',
-   beforeSend: function(request) {
-     request.setRequestHeader('x-access-token', '1d98887a2e12a06a3155e96808793b5e');
-   },
-   url: 'https://api.openuv.io/api/v1/uv?lat=' + lat + '&lng=' + lng + '&alt=' + alt + '&ozone=' + ozone + '&dt=' + dt,
-   success: function(response) {
-     //handle successful response
-   },
-   error: function(response) {
-     // handle error response
-   }
- });
-}
-
-let logValue=getUVIndex();
-console.log(logValue);
+axios.get('https://www.reddit.com/r/javascript.json').then(response => {
+  console.log(response);
+});
 
 import Link from 'next/link'
 const Index = () => (

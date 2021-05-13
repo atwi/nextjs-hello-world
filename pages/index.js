3d80371d776201b1efc437332c5e53cde6a2cda3
Alex Thorburn-Winsor <sputnik4024@gmail.com>
2021-05-13 16:26:47 -0400

    Added API Request

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,9 @@
+import axios from 'axios';
+
+axios.get('https://jsonplaceholder.typicode.com/todos/1').then(response => {
+  console.log(response);
+});
+
 import Link from 'next/link'
 const Index = () => (
   <div>
